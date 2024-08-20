233cc32b4b6b56ee756ac7d86a1164aa0fba1181
Adonay Tobar <tobaradonay@gmail.com>
2024-08-19 19:22:41 -0600

    Agregando cosas

--- main.js
+++ main.js
@@ -1,12 +1,41 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('.get-started-btn');
-    const phoneNumber = "60288548"; // Reemplaza con tu número de teléfono, con el código del país sin signos + ni espacios
+    const phoneNumber = "72757591"; // Reemplaza con tu número de teléfono, con el código del país sin signos + ni espacios
     const message = "Necesito una encomienda";
 
     buttons.forEach(button => {
         button.addEventListener('click', function () {
             const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+            window.open(whatsappUrl, '_blank');
+        });
+    });
+});
+
+
+
+// Creamos un array de objetos que contiene las clases y los mensajes correspondientes
+const buttons = [
+    { selector: '.maxi', message: "Necesito algo de la Maxi" },
+    { selector: '.selectos', message: "Necesito algo del Selectos" },
+    { selector: '.little', message: "Necesito una Pizza" },
+    { selector: '.santo', message: "Necesito algo de Santo Pecado" },
+    { selector: '.integral', message: "Necesito Pan Integral" },
+    { selector: '.fresa', message: "Necesito Fresas con crema" },
+    { selector: '.ale', message: "Necesito comida de Ale" },
+    { selector: '.gena', message: "Necesito algo de Gena Bistro Cafe" },
+    { selector: '.zule', message: "Necesito una pupusas de Zuleima" },
+    { selector: '.fuente', message: "Necesito una comida de La Fuente" },
+    { selector: '.choco', message: "Necesito Chocos Lokos" }
+];
+
+// Iteramos sobre cada botón y le asignamos un event listener
+buttons.forEach(buttonInfo => {
+    const buttonElements = document.querySelectorAll(buttonInfo.selector);
+        const phoneNumber = "72757591"; // Reemplaza con tu número de teléfono, con el código del país sin signos + ni espacios
+    buttonElements.forEach(button => {
+        button.addEventListener('click', () => {
+            const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(buttonInfo.message)}`;
             window.open(whatsappUrl, '_blank');
         });
     });
